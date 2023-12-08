59430b8a024841242b96b47d8fb1bda35e2106e1
RezaMAkbar <rezamakbar2004@gmail.com>
2023-12-08 08:59:06 +0700

    text field bg login

--- Tubes_PBW2/resources/views/obat/tambahObat.blade.php
+++ Tubes_PBW2/resources/views/obat/tambahObat.blade.php
@@ -103,6 +103,13 @@
                                 <button type="reset" class="btn btn-danger">Reset</button>
                             </div>
                         </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ url('/dashboard') }}">
+                                {{ __('Home') }}
+                            </a>
+                            
+                        </div>
                     </form>
                 </div>
             </div>
